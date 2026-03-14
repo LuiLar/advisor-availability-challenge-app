148e2c3e9c9d379c90a90987e28c93f74e7ad51d
Luis Larghi <windinglair@gmail.com>
2026-03-14 16:13:10 -0300

    feat: add responsive design

--- app/Components/Card.tsx
+++ app/Components/Card.tsx
@@ -11,16 +11,18 @@ const Card = ({ advisor }: CardProps): JSX.Element => {
   const { fullName, imageUrl, price } = advisor;
 
   return (
-    <div className="flex justify-between gap-7 py-7 [&:nth-child(n+1):nth-last-child(n+1)]:border-b [&:nth-child(n+1):nth-last-child(n+1)]:border-b-gray-400">
-      <Image
-        width={120}
-        height={120}
-        className="rounded-full aspect-square object-cover"
-        src={imageUrl}
-        alt={fullName}
-      />
+    <div className="flex flex-col md:flex-row justify-between gap-7 py-7 [&:nth-child(n+1):nth-last-child(n+1)]:border-b [&:nth-child(n+1):nth-last-child(n+1)]:border-b-gray-400">
+      <div className="flex gap-7">
+        <Image
+          width={120}
+          height={120}
+          className="rounded-full aspect-square object-cover"
+          src={imageUrl}
+          alt={fullName}
+        />
 
-      <p className="flex-1 text-teal-600 text-xl">{fullName}</p>
+        <p className="flex-1 text-teal-600 text-xl">{fullName}</p>
+      </div>
 
       <div className="flex flex-col gap-2 justify-end">
         <p className="text-lg">
--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default async function Home() {
   const advisors: Advisor[] = await getAdvisors();
 
   return (
-    <main className="mt-20 w-1/2 ml-auto mr-auto">
+    <main className="w-10/12 md:mt-20 md:w-1/2 mx-auto">
       <AdvisorList advisors={advisors} />
     </main>
   );
